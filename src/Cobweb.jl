ced742a38564ac3e8820335ec1b7ce0e1e53b994
Josh Day <emailjoshday@gmail.com>
2022-09-21 07:04:27 -0400

    add propertynames for h so that tags autocomplete

--- src/Cobweb.jl
+++ src/Cobweb.jl
@@ -43,6 +43,7 @@ h(tag, children...; kw...) = Node(tag, get_attrs(kw), collect(children))
 h(tag, attrs::Dict, children...) = Node(tag, attrs, collect(children))
 
 Base.getproperty(::typeof(h), tag::Symbol) = h(string(tag))
+Base.propertynames(::typeof(h)) = HTML5_TAGS
 
 #-----------------------------------------------------------------------------# @h
 HTML5_TAGS = [:a,:abbr,:address,:area,:article,:aside,:audio,:b,:base,:bdi,:bdo,:blockquote,:body,:br,:button,:canvas,:caption,:cite,:code,:col,:colgroup,:data,:datalist,:dd,:del,:details,:dfn,:dialog,:div,:dl,:dt,:em,:embed,:fieldset,:figcaption,:figure,:footer,:form,:h1,:h2,:h3,:h4,:h5,:h6,:head,:header,:hgroup,:hr,:html,:i,:iframe,:img,:input,:ins,:kbd,:label,:legend,:li,:link,:main,:map,:mark,:math,:menu,:menuitem,:meta,:meter,:nav,:noscript,:object,:ol,:optgroup,:option,:output,:p,:param,:picture,:pre,:progress,:q,:rb,:rp,:rt,:rtc,:ruby,:s,:samp,:script,:section,:select,:slot,:small,:source,:span,:strong,:style,:sub,:summary,:sup,:svg,:table,:tbody,:td,:template,:textarea,:tfoot,:th,:thead,:time,:title,:tr,:track,:u,:ul,:var,:video,:wbr]
